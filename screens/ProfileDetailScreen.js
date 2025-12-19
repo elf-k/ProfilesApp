307c1ca0223cd61e6abc0275af1d92137e3ef0b9
elf-k <karakazanelif@gmail.com>
2025-12-19 15:38:32 +0300

    feat: setup stack navigation for profiles

---
+++ screens/ProfileDetailScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function ProfileDetailScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Detail Screen</Text>
+    </View>
+  );
+}
---
+++ screens/ProfilesListScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function ProfilesListScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profiles List Screen</Text>
+    </View>
+  );
+}
